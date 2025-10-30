5338d22cd92c3714872cad8c99eb65d1f31ff4ec
DuvanSanchez12 <duvandcrs@gmail.com>
2025-10-30 16:44:52 -0500

    cambio a php

---
+++ footer.php
@@ -0,0 +1,54 @@
+<footer class="bg-light text-center py-4 mt-5">
+      <h5 class="mb-3 text-dark">Conéctate con nosotros</h5>
+      <div class="d-flex tech-icons justify-content-center">
+        <a
+          href="https://github.com/DuvanSanchez12"
+          target="_blank"
+          class="text-white"
+        >
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+            width="28"
+            alt="GitHub"
+          />
+        </a>
+        <a
+          href="https://www.coursera.org/user/4d70e04ea0a1ec1e42e9a544bf5568cb"
+          target="_blank"
+          class="text-white"
+        >
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
+            width="28"
+            alt="LinkedIn"
+          />
+        </a>
+        <a href="https://www.instagram.com/camilo_sanchez547" target="_blank">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png"
+            width="28"
+            alt="Instagram"
+          />
+        </a>
+        <a href="mailto:ibago@gmail.com" class="text-white">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/281/281769.png"
+            width="28"
+            alt="Gmail"
+          />
+        </a>
+      </div>
+      <p class="mt-4 mb-0 text-secondary small">
+        © 2025 Duvan Sanchez — Todos los derechos reservados.
+      </p>
+    </footer>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/map.js"></script>
+    <script
+      async
+      defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDmlj3lEJjshTIK63cp4NmBf5VageqCzak&callback=initMap"
+    ></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+    <title>IbaGO - Web</title>
+  </head>
+  <body>
+    <header>
+      <?php include 'navbar.php'; ?>
+    </header>
